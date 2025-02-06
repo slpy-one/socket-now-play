03e80c6aaf1cc1409dc3c16d22a9cd7f7979317f
Suphakit P. <me@suphakit.net>
2025-02-07 01:02:05 +0700

    feat<page>: setting up page base

--- src/js/socket.js
+++ src/js/socket.js
@@ -1,15 +1,37 @@
 const socket = new WebSocket("wss://lan.mama.ovh/socket");
 
-const subConfig = `{"op":2,"d":{"subscribe_to_ids":["387465159322632202"]}}`;
+const Element = document.querySelector("#element");
+const ConnectButton = document.querySelector("#connection");
+
+const userID = "387465159322632202";
+
+const subConfig = `{"op":2,"d":{"subscribe_to_ids":["${userID}"]}}`;
+
+let socketConnectState = 1;
 
 socket.addEventListener("open", (e) => {
   console.log("connected");
 });
 
 socket.addEventListener("message", (event) => {
-  console.log(JSON.parse(event.data));
+  let data = JSON.parse(event.data);
+  console.log(data);
+  // console.log(data.op);
+
+  socketConnectState = data.op;
+
+  if (data.op != 0) {
+    ConnectButton.innerHTML = "Connect";
+  } else {
+    ConnectButton.innerHTML = "Disconnect";
+  }
 });
 
 const connectConfig = () => {
-  socket.send(subConfig);
+  if (socket.readyState == 1) {
+    if (socketConnectState == 1) {
+      socket.send(subConfig);
+    } else {
+    }
+  }
 };
