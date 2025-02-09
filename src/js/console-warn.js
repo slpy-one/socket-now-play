1e7e185d560b5a7a05dcad338f1c0ad9a513dd4c
Suphakit P. <me@suphakit.net>
2025-02-09 15:53:45 +0700

    feat<script>: added console warning

---
+++ src/js/console-warn.js
@@ -0,0 +1,8 @@
+console.log(
+  `%cWarning`,
+  "color: whitesmoke; background: #fe6e6f; font-size: 1.5rem; padding: .1rem .5rem; font-family: Tahoma; border-radius: 1rem; font-weight: bold;"
+);
+console.log(`please don't do anything here lah..`);
+console.log("");
+console.log("-----------------------------------------");
+console.log("");
