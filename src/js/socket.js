e4925f33c19b426fb4413bef8fcea38404ee210d
Suphakit P. <me@suphakit.net>
2025-02-10 01:06:11 +0700

    feat<scripts>: added data validation

--- src/js/socket.js
+++ src/js/socket.js
@@ -4,7 +4,23 @@ const ConnectButton = document.querySelector("#connection");
 const socketURI = `wss://lan.mama.ovh/socket`;
 const userID = "387465159322632202";
 const subConfig = `{"op":2,"d":{"subscribe_to_ids":["${userID}"]}}`;
-var payload;
+
+const ElementRenderer = (data) => {
+  var payloadData;
+
+  if (data.t == "INIT_STATE") {
+    payloadData = data.d[userID];
+  } else if (data.t == "PRESENCE_UPDATE") {
+    payloadData = data.d;
+  } else {
+  }
+
+  console.log(payloadData.spotify);
+
+  if (payloadData.spotify != null) {
+  } else {
+  }
+};
 
 var socket = new WebSocket(socketURI);
 
@@ -21,13 +37,13 @@ const socketMessageFunction = (e) => {
     socket.send(subConfig);
   } else if (data.op == 0) {
     payload = data;
+    ElementRenderer(data);
   }
-
-  console.log(data);
 };
 
 const socketCloseFunction = (e) => {
   console.log("disconnected");
+  buttonHandlerFunction("connect");
 };
 
 const buttonHandlerFunction = (e) => {
